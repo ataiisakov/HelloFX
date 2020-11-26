a54a183d5ba6a85e6f802540f5bf0edf5edfa6a7
User <atayisakov558@gmail.com>
2020-11-26 16:27:05 +0100

    Initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test1() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test2() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test3() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test4() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test5() {
+        Assertions.assertTrue(true);
+    }
+}
